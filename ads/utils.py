5e3962fbd7ea9a0a608aa161501a2046a964d6fb
andycasey <andycasey@gmail.com>
2013-09-13 20:26:30 +1000

    API key either via filename or environment variable

--- ads/utils.py
+++ ads/utils.py
@@ -12,7 +12,19 @@ import os
 __all__ = ['get_dev_key']
 
 def get_dev_key():
-    with open(os.path.abspath(os.path.expanduser('~/.ads/dev_key')), 'r') as fp:
-        dev_key = fp.readline().rstrip()
 
-    return dev_key
+    ads_dev_key_filename = os.path.abspath(os.path.expanduser('~/.ads/dev_key'))
+
+    if os.path.exists(ads_dev_key_filename):
+        with open(ads_dev_key_filename, 'r') as fp:
+            dev_key = fp.readline().rstrip()
+
+        return dev_key
+
+    if 'ADS_API_KEY' in os.environ:
+        return os.environ['ADS_API_KEY']
+
+
+    raise IOError("no ADS API key found in ~/.ads/dev_key")
+
+
