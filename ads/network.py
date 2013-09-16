11735fdb1c04e6f1762351eb37a6ef3dabb025f9
andycasey <andycasey@gmail.com>
2013-09-16 14:30:17 +1000

    Added co-authorship network tree

--- ads/network.py
+++ ads/network.py
@@ -7,10 +7,143 @@ from __future__ import division, print_function
 __author__ = "Andy Casey <acasey@mso.anu.edu.au>"
 
 # Standard libary
+import itertools
 import json
 import os
 
-__all__ = ['nodes']
+# Module specific
+from search import search as ads_search
+from utils import unique_preserved_list
+
+__all__ = ['nodes', 'export', 'coauthors']
+
+
+def coauthors(name, depth, author_repr=None, rows=5):
+    """Build a network of co-authors based on the name provided,
+    to a level depth as provided.
+
+    Inputs
+    ------
+    name : str
+        The author name to focus on.
+
+    depth : int
+        The number of depth levels to progress from `name`.
+    """
+
+    try:
+        depth = int(depth)
+    except TypeError:
+        raise TypeError("depth must be an integer-like type")
+
+    if depth < 1:
+        raise ValueError("depth must be a positive integer")
+
+    if author_repr is None:
+        author_repr = lambda x: x
+
+    all_articles = []
+    level_authors = [name]
+    for i in xrange(depth):
+
+        print((i, level_authors))
+        next_level_authors = []
+        for j, author in enumerate(level_authors):
+
+            # If this is a "Collaboration" or "Team",
+            # we should ignore it and just deal with
+            # *real* people.
+
+            if "collaboration" in author.lower() \
+            or " team" in author.lower():
+                continue
+
+            # Get 5 top articles by this author
+            articles = ads_search(u'"{author}"'.format(author=author),
+                fl="author,citation_count", filter="property:refereed", rows=rows if [i, j] != [0, 0] else 5,
+                order="desc", sort="citations")
+
+            # Add these articles to the list
+            all_articles.extend(articles)
+
+            # Put these authors into the next level
+            next_level_authors.extend(sum([article.author for article in articles], []))
+
+        level_authors = []
+        level_authors.extend(next_level_authors)
+
+
+    # Initialise a group with the name input
+    links = []
+    groups = [0]
+    values = []
+    nodes = [author_repr(name)]
+
+    # Remove article double-ups?
+
+    # Go through all articles
+    for group_number, article in enumerate(all_articles, start=1):
+
+        print(article.author)
+        # Make sure each co-author has a node
+        # Limit each paper to 10 authors
+        for co_author in article.author:
+
+            co_author = author_repr(co_author)
+            if co_author not in nodes:
+
+                # Create the node for this author
+                groups.append(group_number)
+                nodes.append(author_repr(co_author))
+
+        # Links should be drawn between all these article.author's,
+        # since they all published together.
+        for (author_one, author_two) in itertools.combinations(article.author, 2):
+            print((author_one, author_two))
+            if name in (author_one, author_two):
+                print("!!!!!!")
+            if author_repr(author_one) == author_repr(author_two):
+                print("skipped")
+                continue
+
+            source = nodes.index(author_repr(author_one))
+            target = nodes.index(author_repr(author_two))
+
+            link = (source, target)
+            knil = (target, source)
+            if link not in links and knil not in links:
+                print("adding link")
+                links.append(link)
+                values.append(1)
+
+            else:
+                print(" ")
+                try:
+                    index = links.index(link)
+                except:
+                    index = links.index(knil)
+
+                values[index] += 1
+
+    # Build formatted nodes and links
+    formatted_nodes = []
+    for author, group in zip(nodes, groups):
+        formatted_nodes.append({"name": author, "group": group})
+
+    formatted_links = []
+    for (source, target), value in zip(links, values):
+        formatted_links.append({
+            "source": source,
+            "target": target,
+            "value": value
+            })
+
+    output = {
+        "nodes": formatted_nodes,
+        "links": formatted_links
+    }
+
+    return output
 
 
 def nodes(articles, attribute, map_func=None):
