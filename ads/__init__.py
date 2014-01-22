c30421c6050e4035c7af56dc297c89d0f2704ebd
andycasey <andycasey@gmail.com>
2014-01-23 04:55:29 +1100

    Hmm

--- ads/__init__.py
+++ ads/__init__.py
@@ -3,7 +3,7 @@
 """ A Python tool for interacting with NASA's ADS system. """
 
 __author__ = "Andy Casey <andy@astrowizici.st>"
-__version__ = "0.0.73"
+__version__ = "0.0.74"
 
 import network
 from search import *
