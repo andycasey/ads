ae350f961248398130e53da27956b7bbe5270c1c
andycasey <andycasey@gmail.com>
2014-01-25 01:41:12 +1100

    Added Bibtex information for some entry types

--- ads/core.py
+++ ads/core.py
@@ -51,8 +51,98 @@ class Article(object):
     def __repr__(self):
         return u"<ads.{0} object at {1}>".format(self.__class__.__name__, hex(id(self)))
 
+    @property
+    def bibtex(self):
+        """Return a BiBTeX entry for the current article."""
 
-    # TODO bibtex @property
+        all_entry_types = "ARTICLE, INPROCEEDINGS, PHDTHESIS, MASTERSTHESIS, NONARTICLE, " \
+            "EPRINT, BOOK, PROCEEDINGS, CATALOG, SOFTWARE".split(", ")
+
+        # Find the entry type
+        entry_type = [entry_type in self.property for entry_type in all_entry_types]
+        if not any(entry_type):
+            raise TypeError("article entry type not recognised")
+
+        entry_type = all_entry_types[entry_type.index(True)]
+
+        # Is it an EPRINT? If so, mark as ARTICLE and use arXiv as journal
+        if entry_type == "EPRINT":
+            entry_type = "ARTICLE"
+
+        elif entry_type in ("NONARTICLE", "CATALOG", "SOFTWARE"):
+            entry_type = "MISC"
+
+        """
+        TYPE = [required, [optional]]
+        
+        article = [author, title, journal, year, [volume, number, pages, month, note, key]]
+        book = [author or editor, title, publisher, year, [volume, series, address, edition, month, note, key]]
+        inproceedings = [author, title, booktitle, year, [editor, pages, organization, publisher, address, month, note, key]]
+        MASTERSTHESIS = [author, title, school, year, [address, month, note, key]]
+        misc = [[author, title, howpublished ,month, year, note, key]]
+        phdthesis = [author, title, school, year, [address, month, note, key]]
+        proceedings = [title, year, [editor, publisher, organization, address, month, note, key]]
+        """
+
+        _ = lambda item: "{{{0}}}".format(item)
+        months = {
+            0: '', 1: 'jan', 2: 'feb', 3: 'mar', 4: 'apr', 5: 'may',
+            6: 'jun', 7: 'jul', 8: 'aug', 9: 'sep', 10: 'oct', 11: 'nov', 12: 'dec'
+        }
+        parsers = {
+            "author":           lambda article: _(" and \n".join([" and ".join(["{{{0}}}, {1}".format(author.split(",")[0], "~".join(["{0}.".format(name[0]) \
+                                    for name in author.split(",")[1].split()])) for author in article.author[i:i+4]]) for i in xrange(0, len(article.author), 4)])),
+            "month":            lambda article: months[int(article.pubdate.split("-")[1])],
+            "pages":            lambda article: _(article.page[0]),
+            "title":            lambda article: "{{{{{0}}}}}".format(article.title[0]),
+            "journal":          lambda article: _(article.pub),
+            "year":             lambda article: _(article.year),
+            "volume":           lambda article: _(article.volume),
+            "adsnote":          lambda article: "{Provided by the SAO/NASA Astrophysics Data System API}",
+            "adsurl":           lambda article: _("http:/adsabs.harvard.edu/abs/{0}".format(article.bibcode)),
+            "keywords":         lambda article: _(", ".join(article.keyword)),
+            "doi":              lambda article: _(article.doi[0]),
+            "eprint":           lambda article: _("arXiv:{0}".format(article.identifier[["astro-" in i for i in article.identifier].index(True)]))
+        }
+
+        # Create start of BiBTeX
+        bibtex = ["@{0}{{{1}".format(entry_type, self.bibcode)]
+            
+        if entry_type == "ARTICLE":
+            required_entries = ["author", "title", "journal", "year"]
+            optional_entries = ["volume", "pages", "month", "adsnote", "adsurl", "doi", "eprint", "keywords"]
+
+        elif entry_type == "BOOK":
+            required_entries = ["author", "title", "publisher", "year"]
+            optional_entries = ["volume", "issue", "month", "adsurl"]
+
+        elif entry_type == "INPROCEEDINGS":
+            required_entries = ["author", "title", "year"]
+            optional_entries = ["pages", "publisher", "month", "adsurl"]
+
+        elif entry_type == "PROCEEDINGS":
+            required_entries = ["title", "year"]
+            optional_entries = ["publisher", "month"]
+
+        else:
+            raise NotImplementedError
+
+        for required_entry in required_entries:
+            try:
+                value = parsers[required_entry](self)
+                if value:
+                    bibtex.append("{0} = {1}".format(required_entry.rjust(9), value))
+            except:
+                raise TypeError("could not generate {0} BibTeX entry for {1}".format(required_entry, self.bibcode))
+
+        for optional_entry in optional_entries:
+            try:
+                value = parsers[optional_entry](self)
+                if value:
+                    bibtex.append("{0} = {1}".format(optional_entry.rjust(9), value))
+            except: pass
+
+        return ",\n".join(bibtex) + "\n}\n"
 
 
     @property
