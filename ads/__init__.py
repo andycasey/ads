77ec0f1dc18befa66f4f0667b4780ee1059015ed
Andy Casey <andycasey@gmail.com>
2014-08-25 11:23:49 +0100

    version bump

--- ads/__init__.py
+++ ads/__init__.py
@@ -4,7 +4,7 @@
 """ A Python tool for interacting with NASA's ADS """
 
 __author__ = "Andy Casey <andy@astrowizici.st>"
-__version__ = "0.0.808dev"
+__version__ = "0.0.809"
 
 import network
 from core import query, metrics, metadata, retrieve_article
