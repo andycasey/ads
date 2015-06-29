1568b5850f607d637c289c8ad60e5fa0a298d1ba
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-06-29 17:56:29 +0000

    SolrResponse: remove setter to .articles; this attr will be read-only

--- ads/models.py
+++ ads/models.py
@@ -55,13 +55,6 @@ class SolrResponse(object):
                 self._articles.append(Article(**doc))
         return self._articles
 
-    @articles.setter
-    def articles(self, value):
-        """
-        transparent .articles setter
-        """
-        self._articles = value
-
 
 class Article(object):
     """
@@ -84,7 +77,6 @@ class Article(object):
         :param kwargs: Set object attributes from kwargs
         """
         self._raw = kwargs
-
         for key, value in kwargs.iteritems():
             setattr(self, key, value)
 
@@ -95,7 +87,6 @@ class Article(object):
         author = self.first_author or "Unknown author"
         if len(self.author_norm) > 1:
             author = "{} et al.".format(author)
-
         return u"<{author} {year}, {bibcode}>".format(
             author=author,
             year=self.year,
@@ -114,104 +105,12 @@ class Article(object):
     @property
     def bibtex(self):
         """Return a BiBTeX entry for the current article."""
-
-        all_entry_types = "ARTICLE, INPROCEEDINGS, PHDTHESIS, MASTERSTHESIS, "\
-            "NONARTICLE, EPRINT, BOOK, PROCEEDINGS, CATALOG, SOFTWARE".split(", ")
-
-        # Find the entry type
-        entry_type = [entry_type in self.property for entry_type in all_entry_types]
-        if not any(entry_type):
-            raise TypeError("article entry type not recognised")
-
-        entry_type = all_entry_types[entry_type.index(True)]
-
-        # Is it an EPRINT? If so, mark as ARTICLE and use arXiv as journal
-        if entry_type == "EPRINT":
-            entry_type = "ARTICLE"
-
-        elif entry_type in ("NONARTICLE", "CATALOG", "SOFTWARE"):
-            entry_type = "MISC"
-
-        """
-        TYPE = [required, [optional]]
-
-        article = [author, title, journal, year, [volume, number, pages, month, note, key]]
-        book = [author or editor, title, publisher, year, [volume, series, address, edition, month, note, key]]
-        inproceedings = [author, title, booktitle, year, [editor, pages, organization, publisher, address, month, note, key]]
-        MASTERSTHESIS = [author, title, school, year, [address, month, note, key]]
-        misc = [[author, title, howpublished ,month, year, note, key]]
-        phdthesis = [author, title, school, year, [address, month, note, key]]
-        proceedings = [title, year, [editor, publisher, organization, address, month, note, key]]
-        """
-
-        _ = lambda item: "{{{0}}}".format(item)
-        months = {
-            0: '', 1: 'jan', 2: 'feb', 3: 'mar', 4: 'apr', 5: 'may',
-            6: 'jun', 7: 'jul', 8: 'aug', 9: 'sep', 10: 'oct', 11: 'nov', 12: 'dec'
-        }
-        parsers = {
-            "author":           lambda article: _(" and \n".join([" and ".join(["{{{0}}}, {1}".format(author.split(",")[0], "~".join(["{0}.".format(name[0]) \
-                                    for name in author.split(",")[1].split()])) for author in article.author[i:i+4]]) for i in xrange(0, len(article.author), 4)])),
-            "month":            lambda article: months[int(article.pubdate.split("-")[1])],
-            "pages":            lambda article: _(article.page[0]),
-            "title":            lambda article: "{{{{{0}}}}}".format(article.title[0]),
-            "journal":          lambda article: _(article.pub),
-            "year":             lambda article: _(article.year),
-            "volume":           lambda article: _(article.volume),
-            "adsnote":          lambda article: "{Provided by the SAO/NASA Astrophysics Data System API}",
-            "adsurl":           lambda article: _("http:/adsabs.harvard.edu/abs/{0}".format(article.bibcode)),
-            "keywords":         lambda article: _(", ".join(article.keyword)),
-            "doi":              lambda article: _(article.doi[0]),
-            "eprint":           lambda article: _("arXiv:{0}".format(article.identifier[["astro-" in i for i in article.identifier].index(True)]))
-        }
-
-        # Create start of BiBTeX
-        bibtex = ["@{0}{{{1}".format(entry_type, self.bibcode)]
-
-        if entry_type == "ARTICLE":
-            required_entries = ["author", "title", "journal", "year"]
-            optional_entries = ["volume", "pages", "month", "adsnote", "adsurl", "doi", "eprint", "keywords"]
-
-        elif entry_type == "BOOK":
-            required_entries = ["author", "title", "publisher", "year"]
-            optional_entries = ["volume", "issue", "month", "adsurl"]
-
-        elif entry_type == "INPROCEEDINGS":
-            required_entries = ["author", "title", "year"]
-            optional_entries = ["pages", "publisher", "month", "adsurl"]
-
-        elif entry_type == "PROCEEDINGS":
-            required_entries = ["title", "year"]
-            optional_entries = ["publisher", "month"]
-
-        else:
-            # We should retrieve it from the ADS page.
-            raise NotImplementedError
-
-        for required_entry in required_entries:
-            try:
-                value = parsers[required_entry](self)
-                if value:
-                    bibtex.append("{0} = {1}".format(required_entry.rjust(9), value))
-            except:
-                raise TypeError("could not generate {0} BibTeX entry for {1}".format(
-                    required_entry, self.bibcode))
-
-        for optional_entry in optional_entries:
-            try:
-                value = parsers[optional_entry](self)
-                if value:
-                    bibtex.append("{0} = {1}".format(optional_entry.rjust(9), value))
-            except: pass
-
-        return ",\n".join(bibtex) + "\n}\n"
-
+        raise NotImplementedError
 
     @property
     def references(self):
         """Retrieves reference list for the current article and stores them."""
         raise NotImplementedError
-
 
     @property
     def citations(self):
@@ -222,7 +121,6 @@ class Article(object):
     def metrics(self):
         """Retrieves metrics for the current article and stores them."""
         raise NotImplementedError
-
 
     def build_reference_tree(self, depth=1, **kwargs):
         """
@@ -245,7 +143,6 @@ class Article(object):
 
         raise NotImplementedError
 
-
     def build_citation_tree(self, depth=1, **kwargs):
         """
         Builds a citation tree for this paper.
@@ -266,3 +163,21 @@ class Article(object):
         """
 
         raise NotImplementedError
+
+
+class SolrQuery(object):
+    """
+    Represents a query to apache solr
+    """
+    pass
+
+
+
+
+class Query(SolrQuery):
+    """
+    Represents an arbitrary query to the adsws-api
+    """
+    def __init__(self):
+        pass
+
--- ads/tests/test_models.py
+++ ads/tests/test_models.py
@@ -15,42 +15,50 @@ class TestSolrResponse(unittest.TestCase):
     """
     Test the SolrResponse object
     """
+    def setUp(self):
+        """
+        setup this test with a mocked solr response via http
+        """
+        with MockSolrResponse('http://solr-response.unittest'):
+            self.response = requests.get('http://solr-response.unittest')
+
     def test_init(self):
         """
         ensure that an init of SolrResponse has the expected keys,
         and that if critical data are missing then raise SolrResponseParseError
         """
 
-        # Load stubdata via a http workflow
-        with MockSolrResponse('http://solr-response.unittest'):
-            response = requests.get('http://solr-response.unittest')
-
-        sr = SolrResponse(response.text)
+        sr = SolrResponse(self.response.text)
         self.assertIn('responseHeader', sr.json)
         self.assertIn('response', sr.json)
         self.assertEqual(sr.numFound, 1)
 
-        malformed_text = response.text.replace(
+        malformed_text = self.response.text.replace(
             'responseHeader',
             'this_is_now_malformed_data',
         )
         with self.assertRaises(SolrResponseParseError):
             SolrResponse(malformed_text)
 
+    def test_articles(self):
+        """
+        the article attribute should be read-only, and set the first time
+        it is called
+        """
+        sr = SolrResponse(self.response.text)
+        self.assertIsNone(sr._articles)
+        self.assertEqual(len(sr.articles), 1)
+        self.assertEqual(sr._articles, sr.articles)
+        with self.assertRaises(AttributeError):
+            sr.articles = 'this should be read-only'
+
     def test_load_http_response(self):
         """
         the classmethod load_http_response() should return an instansiated
         SolrResponse class that has an .articles attribute that are Article
         objects
-
-        this test also excersices the @articles property
         """
-
-        # Load stubdata via a http workflow
-        with MockSolrResponse('http://solr-response.unittest'):
-            response = requests.get('http://solr-response.unittest')
-
-        sr = SolrResponse.load_http_response(response)
+        sr = SolrResponse.load_http_response(self.response)
         self.assertEquals(len(sr.articles), 1)
         self.assertIsInstance(sr.articles[0], Article)
         self.assertEqual(sr.articles[0].doi, [u'10.1051/0004-6361/201321247'])
@@ -63,8 +71,8 @@ class TestArticle(unittest.TestCase):
 
     def setUp(self):
         """
-        Set up this test by loading stub data via the SolrResponse class using
-        an http interface --> dependency on ads.models.SolrResponse
+        Set up these tests by loading stub data via the SolrResponse class
+        using an http interface --> dependency on ads.models.SolrResponse
         """
         with MockSolrResponse('http://solr-response.unittest'):
             response = requests.get('http://solr-response.unittest')
@@ -73,6 +81,31 @@ class TestArticle(unittest.TestCase):
     def test_init(self):
         """
         after init, the attributes _references and _citations should be None,
-        _raw should be equal to
+        _raw should be a dict containing a subset of all class attributes
         """
-        pass
+        self.assertIsNone(self.article._references)
+        self.assertIsNone(self.article._citations)
+        self.assertIsNone(self.article._bibtex)
+        for key, value in self.article._raw.iteritems():
+            self.assertEqual(
+                self.article.__getattribute__(key),
+                value,
+                msg="Instance attribute and _raw mismatch on {}".format(key)
+            )
+
+    def test_print_methods(self):
+        """
+        the class should return a user-friendly formatted identified when the
+        __str__ or __unicode__ methods are called
+        """
+        self.assertEqual(
+            '<Sudilovsky, V. et al. 2013, 2013A&A...552A.143S>',
+            self.article.__str__()
+        )
+        self.assertEqual(self.article.__unicode__(), self.article.__str__())
+
+
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
