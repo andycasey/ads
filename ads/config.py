b5a6bca43bbf59e84039822d7e285fdbe7f1363e
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-06-29 19:07:02 +0000

    config: add URL config

---
+++ ads/config.py
@@ -0,0 +1,9 @@
+"""
+configuration
+"""
+
+ADSWS_API_URL = 'https://api.adsabs.harvard.edu/v1'
+SEARCH_URL = '{}/search/query/'.format(ADSWS_API_URL)
+BIGQUERY_URL = '{}/search/bigquery/'.format(ADSWS_API_URL)
+METRICS_URL = '{}/metrics/'.format(ADSWS_API_URL)
+EXPORT_URL = '{}/export/'.format(ADSWS_API_URL)
