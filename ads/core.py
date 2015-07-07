e858529d0c5ad5125fa5eb27c4593dd66161f3a2
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-07-07 17:23:16 +0000

    SearchQuery: Implement default fl

--- ads/core.py
+++ ads/core.py
@@ -263,9 +263,10 @@ class SearchQuery(BaseQuery):
     Represents a query to apache solr
     """
     HTTP_ENDPOINT = SEARCH_URL
+    DEFAULT_FIELDS = ["author", "first_author", "bibcode", "id", "year"]
 
-    def __init__(self, query_dict=None, q=None, fq=None, fl=None, sort=None,
-                 start=0, rows=50, max_pages=3, **kwargs):
+    def __init__(self, query_dict=None, q=None, fq=None, fl=DEFAULT_FIELDS,
+                 sort=None, start=0, rows=50, max_pages=3, **kwargs):
         """
         constructor
         :param query_dict: raw query that will be sent unmodified. raw takes
