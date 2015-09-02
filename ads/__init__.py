6c57c2d920c1cb88d4cd49d7e6325b62e0512a28
Andy Casey <andycasey@gmail.com>
2015-09-02 12:44:05 +0100

    Include requirements.txt to fix a pip install bug

--- ads/__init__.py
+++ ads/__init__.py
@@ -4,7 +4,7 @@
 """ A Python tool for interacting with NASA's ADS """
 
 __author__ = "Andy Casey <andy@astrowizici.st>"
-__version__ = "0.11"
+__version__ = "0.11.1"
 
 from .metrics import MetricsQuery
 from .export import ExportQuery
