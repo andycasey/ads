896d711f7a437b365f5bc3fe9abf05c2ba272480
Andy Casey <andycasey@gmail.com>
2015-11-25 08:39:01 +0000

    Ensure article ID is always retrieved (#33)

--- ads/__init__.py
+++ ads/__init__.py
@@ -4,8 +4,9 @@
 """ A Python tool for interacting with NASA's ADS """
 
 __author__ = "Andy Casey <andy@astrowizici.st>"
-__version__ = "0.11.1"
+__version__ = "0.11.2"
 
 from .metrics import MetricsQuery
 from .export import ExportQuery
 from .search import SearchQuery, query
+#from .libraries import LibraryQuery, Library #soon
