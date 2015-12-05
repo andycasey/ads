5445dc48d898618890a4da53f843d94fd6110eeb
Vladimir Sudilovsky <vsudilovsky@gmail.com>
2015-12-04 23:56:21 -0500

    tests: fix iter tests

--- ads/tests/test_search.py
+++ ads/tests/test_search.py
@@ -131,13 +131,13 @@ class TestSearchQuery(unittest.TestCase):
             self.assertEqual(len(sq.articles), 1)
             self.assertEqual(sq._query['start'], 1)
             self.assertEqual(next(sq).bibcode, '2012GCN..13229...1S')
-            self.assertEqual(len(list(sq)), 19)  # 2 already returned
+            self.assertEqual(len(list(sq)), 18)  # 2 already returned
             with self.assertRaisesRegexp(
                     StopIteration,
                     "Maximum number of pages queried"):
                 next(sq)
             sq.max_pages = 500
-            self.assertEqual(len(list(sq)), 28-19-2)
+            self.assertEqual(len(list(sq)), 28-18-2)
             with self.assertRaisesRegexp(
                     StopIteration,
                     "All records found"):
