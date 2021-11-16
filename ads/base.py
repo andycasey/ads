4291f8ad32b75bd72ccc25d6efaab5a9a6cf9838
Andy Casey <andycasey@gmail.com>
2021-11-16 12:09:23 +1100

    better rate limit handling

--- ads/base.py
+++ ads/base.py
@@ -1,3 +1,4 @@
+import collections
 import aiohttp
 import asyncio
 import os
@@ -8,24 +9,12 @@ import warnings
 from ads import logger, config
 from .exceptions import APIResponseError
 
-#from ads import config
-#from ads import __version__
-__version__ = "0.2.0" # TODO
-
-TOKEN_FILES = list(map(os.path.expanduser,
-    [
-        "~/.ads/token",
-        "~/.ads/dev_key",
-    ]
-))
-TOKEN_ENVIRON_VARS = ["ADS_API_TOKEN", "ADS_DEV_KEY"]
-
-
+__version__ = "0.2.0"
 
 
 class BaseQuery(object):
 
-    _token = None #config.token # TODO
+    _token = config.token
     _limit_per_host = 10
         
     @property
@@ -39,29 +28,30 @@ class BaseQuery(object):
         - `ads.config.token`
         """
         if self._token is None:
-            for v in map(os.environ.get, TOKEN_ENVIRON_VARS):
+            for v in map(os.environ.get, config.TOKEN_ENVIRON_VARS):
                 if v is not None:
                     self._token = v
                     return self._token
-            for f in TOKEN_FILES:
+            for f in config.TOKEN_FILES:
                 try:
                     with open(f) as fp:
                         self._token = fp.read().strip()
                         return self._token
                 except IOError:
                     pass
-            # TODO: Allow this.
-            #if ads.config.token is not None:
-            #    self._token = ads.config.token
-            #    return self._token
-            warnings.warn("No token found", RuntimeWarning)
+            if config.token is not None:
+                self._token = config.token
+                return self._token
+            warnings.warn(
+                "No SAO/NASA ADS API token found. "
+                "See https://ads.readthedocs.io/en/v1/user/api-key.html for more details.", 
+                RuntimeWarning
+            )
         return self._token
-
 
     @token.setter
     def token(self, value):
         self._token = value
-
 
     @property
     def request_headers(self):
@@ -71,7 +61,6 @@ class BaseQuery(object):
             "Content-Type": "application/json"
         }
 
-
     @property
     def async_session(self):
         """ A client session for performing asynchronous HTTP requests. """
@@ -79,13 +68,12 @@ class BaseQuery(object):
             try:
                 return self._async_session
             except AttributeError:
-                # We are going to be good samaratins and limit the number of simultaneous connections.
+                # We are going to be good citizens and limit the number of simultaneous connections.
                 # (If we don't, ADS will do it anyways.)
                 self._async_session = aiohttp.ClientSession(
                     connector=aiohttp.TCPConnector(limit_per_host=self._limit_per_host),
                     headers=self.request_headers
                 )
-
 
     @property
     def session(self):
@@ -97,19 +85,20 @@ class BaseQuery(object):
                 self._session = requests.session()
                 self._session.headers.update(self.request_headers)
 
-
     # Synchronous context manager.
 
     def __enter__(self):
         return self
     
     def __exit__(self, *args):
+        return self.__del__()
+
+    def __del__(self):
         try:
             self.session.close()
         finally:
             return None
 
-    
     def api_request(self, end_point, method="GET", **kwargs):
         """
         Perform a synchronous API request.
@@ -132,15 +121,15 @@ class BaseQuery(object):
         available_methods = ("get", "post", "put", "delete")
         cleaned_method = method.lower().strip()
         if cleaned_method not in available_methods:
-            raise ValueError(f"Method '{method}' not available. Available methods: {', '.join(available_methods)}")
+            raise ValueError(
+                f"Method '{method}' not available. "
+                f"Available methods: {', '.join(available_methods)}"
+            )
         method_func = getattr(self.session, cleaned_method)
-
         return APIResponse.load_http_response(method_func(url, **kwargs))
-
 
     def _api_url(self, end_point):
         return "/".join(map(lambda _: _.strip("/"), [config.ADSWS_API_URL, end_point]))
-
 
     async def async_api_request(self, end_point, params, method="GET", **kwargs):
         
@@ -171,40 +160,76 @@ class Singleton(type):
 
 class RateLimits(object, metaclass=Singleton):
 
-    response_to_query = {
-        'SolrResponse': 'SearchQuery',
-        'MetricsResponse': 'MetricsQuery',
-        'ExportResponse': 'ExportQuery'
+    # May need to get explicit information from ADS team on how end points
+    # and service limits relate.
+    services = {
+        "search": "solr",
+        "biblib": "biblib",
+        "export": "export",
+        "metrics": "metrics",
     }
+    limits = {}
+        
+    @classmethod
+    def get_rate_limits(cls, service):
+        return cls().limits[service]
+        
+    @classmethod
+    def get_service(cls, url):
+        """
+        Return the ADS service given a URL.
 
-    def __init__(self, name):
-        self.limits = {}
-        self.name = self.response_to_query.get(name, name)
+        :param url:
+            The requested URL.
+        """
+        end_point = url[len(config.ADSWS_API_URL):].lstrip("/")
+        collection = end_point.split("/")[0]
+        try:
+            return cls.services[collection]
+        except:
+            raise KeyError(f"Cannot infer service from end point {end_point} ({url})")
 
     @classmethod
-    def getRateLimits(cls, name):
-        return cls(cls.response_to_query.get(name, name))
+    def set_from_http_response(cls, http_response):
+        """
+        Set the current rate limits from the given HTTP response.
+        
+        :param service:
+            The ADS service name.
 
-
-    def set(self, response):
-        self.limits.update({
-            'limit': response.headers.get('x-ratelimit-limit', ''),
-            'remaining': response.headers.get('x-ratelimit-remaining', ''),
-            'reset': response.headers.get('x-ratelimit-reset', ''),
-        })
+        :param http_response:
+            The HTTP response.
+        """
+        service = cls.get_service(http_response.url)
+        cls().set(
+            service, 
+            **{
+                'limit': http_response.headers.get('x-ratelimit-limit', None),
+                'remaining': http_response.headers.get('x-ratelimit-remaining', None),
+                'reset': http_response.headers.get('x-ratelimit-reset', None),
+            }
+        )
         # TODO: Does this apply to ExportQuery and MetricsQuery?
         #if "max_rows" not in self.limits and has_multiple_pages(response):
         #    self.limits["max_rows"] = response.json()["responseHeader"]["para"]
         
+    def set(self, service, **kwargs):
+        """
+        Set the limits for an ADS service.
+        
+        :param service:
+            The service name (e.g., solr).
+        
+        :param kwargs:
+            The keyword arguments to set for the limits (e.g., limit, remaining, reset).
+        """
+        self.limits.setdefault(service, {}).update(kwargs)
 
     def to_dict(self):
         return self.limits
 
     def __str__(self):
-        return '{}: {}'.format(
-            self.name,
-            json.dumps(self.limits)
-        )
+        return json.dumps(self.limits)
 
 
 def has_multiple_pages(response):
@@ -217,21 +242,16 @@ def has_multiple_pages(response):
     return False
 
 
-    
-
-
-
 class APIResponse(object):
-    """
-    A response from an ADS API end point.
-    
-    """
+
+    """ A response from an ADS API end point. """
+
     response = None
 
     @classmethod
     def load_http_response(cls, http_response):
-
         if not http_response.ok:
+            # Try to give an informed error message.
             try:
                 raise APIResponseError(http_response.json()["error"])
             except (KeyError, AttributeError, TypeError, ValueError):
@@ -240,11 +260,14 @@ class APIResponse(object):
         c = cls(http_response)
         c.response = http_response
 
-        # TODO: Update rate-limits.  
+        # We used to get rate limits from the class name, and have inherited classes
+        # for all different service requests and response. 
+        # But that meant response objects that had inconsistent attributes, and some
+        # services only have one end point.
+        RateLimits.set_from_http_response(http_response)
+
         return c
 
-    
     def __init__(self, http_response):
         self._raw = http_response
         self.json = http_response.json()
-
