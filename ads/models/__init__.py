29b5a7d1decb714e15f00719343767309490a778
Andy Casey <andycasey@gmail.com>
2021-12-07 11:29:06 +1100

    orm models

--- ads/models/__init__.py
+++ ads/models/__init__.py
@@ -1,3 +1,3 @@
 from ads.models.document import Document
-from ads.models.affiliation import Affiliation, Country
+from ads.models.affiliation import Affiliation
 from ads.models.journal import Journal
--- ads/models/journal.py
+++ ads/models/journal.py
@@ -1,9 +1,9 @@
 """
 Journal data model.
 """
-from ads.models.local import LocalModel
-
 from peewee import TextField
+
+from ads.models.local import LocalModel
 
 
 class Journal(LocalModel):
@@ -11,23 +11,4 @@ class Journal(LocalModel):
     """ A journal produced by a publisher. """
 
     abbreviation = TextField(primary_key=True) 
-    full_name = TextField()
-
-
-if __name__ == "__main__":
-
-    from ads.models.local import database
-
-    database.connect()
-    database.create_tables([Journal], safe=True)
-    database.close()
-
-
-    import json
-    with open("journals.json", "r") as fp:
-        journals = json.load(fp)
-    
-    for abbreviation, full_name in journals.items():
-        with database.atomic():
-            Journal.create(abbreviation=abbreviation, full_name=full_name)
-    
+    title = TextField()
