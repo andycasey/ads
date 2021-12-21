2de3dfa13d488df5255e2f1c082dd656121bb4c8
Andy Casey <andycasey@gmail.com>
2021-12-21 17:29:26 +1100

    add test for lazy load

---
+++ ads/tests/models/test_lazy.py
@@ -0,0 +1,25 @@
+import unittest
+from ads.models.document import Document
+
+class TestExpressions(unittest.TestCase):
+
+    def test_lazy_load(self):
+        year = 2005
+
+        fields = (Document.title, Document.author)
+        query = Document.select(*fields)\
+                        .where(Document.year == year)\
+                        .limit(1)
+
+        doc, = list(query)
+        self.assertNotIn("year", doc.__data__)
+
+        # It should have bibcode and id, even though we did
+        # not ask for them, because these are necessary.
+        self.assertIn("id", doc.__data__)
+        self.assertIn("bibcode", doc.__data__)
+
+        # Now let's try execute a lazy load.
+        self.assertEqual(doc.year, year)
+        self.assertIn("year", doc.__data__)
+        
