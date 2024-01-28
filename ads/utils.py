eb5d1645966a6fe6945b9a1ae445e49333a64737
Andy Casey <andycasey@gmail.com>
2024-01-29 09:29:55 +1100

    force conflict merge for 117 because gh can't handle it

--- ads/utils.py
+++ ads/utils.py
@@ -3,21 +3,19 @@ Utilities and helpers
 """
 
 import warnings
-
-from werkzeug._internal import _missing
 from werkzeug.utils import cached_property as _cached_property
+from werkzeug._internal import _missing
 
 
 class cached_property(_cached_property):
     """
     Wrap cached property to print relevant warnings
     """
-
     def __init__(self, func, name=None, doc=None):
         super(cached_property, self).__init__(func, name, doc)
 
     def __get__(self, obj, type=None):
-        # Essentially copying to insert a message....:
+        # Essentially copying to insert a message....: 
         # https://github.com/pallets/werkzeug/blob/master/werkzeug/utils.py
 
         if obj is None:
@@ -25,22 +23,13 @@ class cached_property(_cached_property):
         value = obj.__dict__.get(self.__name__, _missing)
         if value is _missing:
             # One time warning that the user is using lazy loading
-            try:
-                name = self.func.__name__
-            except AttributeError:
-                # for python versions at least >= 3.9.6
-                name = self.fget.__name__
             warnings.warn(
                 "You are lazy loading attributes via '{}', and so are "
                 "making multiple calls to the API. This will impact your overall "
-                "rate limits.".format(name),
+                "rate limits."
+                .format(self.__name__),
                 UserWarning,
             )
-
-            try:
-                value = self.func(obj)
-            except AttributeError:
-                # for python versions at least >= 3.9.6
-                value = self.fget(obj)
+            value = self.fget(obj)
             obj.__dict__[self.__name__] = value
         return value
